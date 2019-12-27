665e1176c93648ec9353faa8c02f3cf4c24f2f1c
dmclavel <dmclavel@up.edu.ph>
2019-12-27 16:48:16 +0800

    Initial commit to master

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
